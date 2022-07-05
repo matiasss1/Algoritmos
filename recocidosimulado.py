f0c8005e2417b9983ee34f4424318837775e7b30
matiasss1 <mmillacura2020@alu.uct.cl>
2022-07-04 20:37:39 -0400

    avance
    distancia euclidea y ploteo de datos de matriz 20

--- recocidosimulado.py
+++ recocidosimulado.py
@@ -1,1 +1,32 @@
-print("hola")
+from turtle import distance
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+df = pd.read_csv("Matriz20.csv") #cargamos .csv
+df = df.drop(df.columns[[0]], axis='columns') # borramos primera columna del .csv 
+
+my_plot = df.plot("0", "1", kind="scatter")
+#plt.show() 
+
+df_i = df.drop(df.columns[[1]], axis='columns')
+df_j = df.drop(df.columns[[0]], axis='columns')
+
+i = df_i.values #guardamos valores de i en un arreglo
+j = df_j.values #guardamos valores de j en un arreglo
+
+p1 = [i[1], j[2]]
+p2 = [i[19], j[19]]
+
+for e in range(len(i)):
+    for h in range(len(j)):
+        p = [i[e],j[h]] # 1,1 1,2 1,3 1,4 
+        pe = [i[e], j[e]] # punto fijo 1,1 2,2 3,3, 4,4, 
+        dis = math.dist(p,pe)
+        if dis > 1.0:   
+            print(dis, p, pe)
+            inicio = [i[0],j[1]]
+            plt.scatter(p, pe, marker= "*")
+            plt.scatter(i[0], j[0], marker = "*")
+plt.show()
