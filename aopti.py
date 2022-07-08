d24fefcbbf415f06f02164fb71fdfcf4128dfc81
matiasss1 <mmillacura2020@alu.uct.cl>
2022-07-08 00:20:43 -0400

    2322

--- aopti.py
+++ aopti.py
@@ -59,9 +59,19 @@ temp = 10
 # perform the simulated annealing search
 best, score, scores = simulated_annealing(objective, bounds, n_iterations, step_size, temp)
 print('Done!')
-print('f(%s) = %f' % (best, score))
-# line plot of best scores
-pyplot.plot(scores, '.-')
-pyplot.xlabel('Improvement Number')
-pyplot.ylabel('Evaluation f(x)')
+
+# explore temperature vs algorithm iteration for simulated annealing
+from matplotlib import pyplot
+# total iterations of algorithm
+iterations = 100
+# initial temperature
+initial_temp = 10
+# array of iterations from 0 to iterations - 1
+iterations = [i for i in range(iterations)]
+# temperatures for each iterations
+temperatures = [initial_temp/float(i + 1) for i in iterations]
+# plot iterations vs temperatures
+pyplot.plot(iterations, temperatures)
+pyplot.xlabel('Iteration')
+pyplot.ylabel('Temperature')
 pyplot.show()
--- definitivo.py
+++ definitivo.py
@@ -1,7 +1,9 @@
+from types import NoneType
 import numpy as np
 from numpy.random import randn, rand, seed
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 from sympy import *
 from scipy.spatial import distance
 
@@ -10,81 +12,25 @@ df = df.drop(df.columns[[0]], axis='columns') # borramos primera columna del .cs
 
 x = df.to_numpy()
 
-
-
-
-area = np.asarray([[0, 1.0]])
+g_sol = np.ndarray[0,1]
 matriz_distancia = []
-
 def funcion_distancia(i, j):
     distancia = distance.euclidean(i,j) # un punto
     return (distancia)
 
 for e in range(len(x)):
     for k in range(len(x)):
-        if e < k: 
+        if e < k:
             punto1 = x[e][0],x[e][1]
             punto2 = x[k][0],x[k][1]
             d = funcion_distancia(punto1,punto2)
-            #matriz_distancia.append([punto1,punto2,d])
+            matriz_distancia.append([punto1,punto2,d])
 #print(matriz_distancia)
-
 def fitness(i, j, d):
-    H = i*j*d
+    H =  x[i]*x[j]*d
     return (H)
-
-def soluciones():
-    aPunto_opt = []
-    for e in range(len(x)):
-        punto_optimo = x[e][0],x[e][1]
-        aPunto_opt.append(punto_optimo)
-    return aPunto_opt
-
-def generar_vecino():
-    numpy_float_arr = np.random.rand (1, 2)
-    return numpy_float_arr
-
-g = soluciones() # aca ira el punto optimo generado de la funcion solucion aleatoria
-
-v = generar_vecino()
-#v1 = v[0][0]
-#v2 = v[0][1]
-
-aZ = [] #arreglo de soluciones evaluadas en funcion optima
-
-for e in range(len(g)):
-    z = fitness(g[e][0],g[e][1], funcion_distancia(g[e][0],g[e][1])) #entregar solucion aleatoria
-    aZ.append(z)
-k = 0
-kmax = 1000 
-t = 1000
-dT = 8
-gn= 0 
-
-while(k < kmax):
-    G_vecino = generar_vecino() #generamos vecino a partir de nuestro punto optimo aleatorio
-    v1 = G_vecino[0][0]
-    v2 = G_vecino[0][1]  
-
-    if gn < 21:
-        Z_vecino = fitness(v1,v2, funcion_distancia((v1,v2),(g[gn])))#evaluamos g vecino en funcion optima
-    else:
-        gn = 0
-    print("largo de z", z)
-    print("soy z vecino", Z_vecino, "largo z vecino ")
-    differencia = Z_vecino - z
-    t = t / (float(k+1))
-    metropolis = exp((- differencia)/t)
-    print("yo soy metropolis", metropolis   )
-    if Z_vecino < z:
-       g = G_vecino
-       z = Z_vecino
-    else: 
-        if rand() < metropolis:
-          g = G_vecino
-          z = Z_vecino 
-    k=k+1
-    gn=gn+21
-    t=t*dT
-
-
+    
+for e in range(len(x)):
+    for k in range(len(x)):
+        if e < k:
+            dh = fitness(e,k,funcion_distancia(e,k))
